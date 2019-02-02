7540e44ef3348672f9345a43a929a99829cc3eca
cjyfff <631127458@qq.com>
2019-02-02 21:25:15 +0800

    add SpringUtils

---
+++ c-delay-queue/src/main/java/com/cjyfff/dq/common/SpringUtils.java
@@ -0,0 +1,29 @@
+package com.cjyfff.dq.common;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jiashen on 2019/2/2.
+ */
+@Component
+public class SpringUtils implements ApplicationContextAware {
+    private static ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtils.applicationContext == null) {
+            SpringUtils.applicationContext = applicationContext;
+        }
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+}
