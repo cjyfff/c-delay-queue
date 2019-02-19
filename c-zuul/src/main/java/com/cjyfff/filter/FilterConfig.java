71bb78945d229b4770ab22f5616d3d1d474ae2cf
褚嘉燊 <631127458@qq.com>
2019-02-19 16:47:24 +0800

    add RateLimitFilter

--- c-zuul/src/main/java/com/cjyfff/filter/FilterConfig.java
+++ c-zuul/src/main/java/com/cjyfff/filter/FilterConfig.java
@@ -7,4 +7,8 @@ public interface FilterConfig {
     String AUTH_FILTER_TYPE = "pre";
 
     int AUTH_FILTER_ORDER = 0;
+
+    String RATE_LIMIT_FILTER_TYPE = "pre";
+
+    int RATE_LIMIT_FILTER_ORDER = 1;
 }
