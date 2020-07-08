1a07c896d72ca7624f86fe200780c3a074d6dd41
Jackson Chu <631127458@qq.com>
2020-07-08 16:59:49 +0800

    Update start.sh

--- c-delay-queue/scripts/start.sh
+++ c-delay-queue/scripts/start.sh
@@ -2,5 +2,5 @@
 
 nohup java -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled \
 -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:garbage-collection.log -XX:+HeapDumpOnOutOfMemoryError \
--XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -Djava.util.concurrent.ForkJoinPool.common.parallelism=16 -d64 -server -Xms256m -Xmx512m -Xmn256m \
+-XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -Djava.util.concurrent.ForkJoinPool.common.parallelism=16 -d64 -server -Xms512m -Xmx512m -Xmn256m \
 -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=128m -jar ./c-delay-queue-0.0.1-SNAPSHOT.jar &
